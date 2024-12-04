2dc984c81312a43fb5d9379db538e0814ddf6572
kannamsairam <155511658+kannamsairam@users.noreply.github.com>
2024-12-04 12:08:34 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://sport-quiz-backend.onrender.com';
+const API_URL = 'https://sport-quiz-backend.onrender.com/';
 
 export const getQuestions = async (sport) => {
     try {
